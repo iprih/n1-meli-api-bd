5c8f7dc9ea4e47e21fa4920af43b0a29a8431bc0
iprih <priscila.silva076@gmail.com>
2019-11-11 22:09:32 -0300

    importando informações do mongo

--- src/model/alunas.js
+++ src/model/alunas.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
 const AlunasSchema = new mongoose.Schema({
-    
+    nome: {type: String},
+    dateOfBirth: {type: String},
+    nasceuEmSp: {type: Boolean},
+    livros:{Array}
+
+
 })
