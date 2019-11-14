066e91f4c55bf9ace3e76b1bce88e2154cbba956
Priscila Silva <43504804+iprih@users.noreply.github.com>
2019-11-13 22:25:56 -0300

    Update alunasController.js

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -12,7 +12,70 @@ exports.get = (req, res) => {
     res.status(200).send(alunas)
   })
 }
+exports.get = (req, res) => {
+  Alunas.find((err, alunas) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).send(alunas);
+  });
+}
 
+exports.getById = (req, res) => {
+  const id = req.params.id
+  Alunas.findById(id, (err, aluna) => {
+    if (err) return res.status(500).send(err);
+    if (!aluna) {
+      return res.status(500).send({ message: `Não localizei a aluna de id ${id}` });
+    }
+    res.status(200).send(aluna);
+  });
+}
+
+exports.getBooks = (req, res) => {
+  const id = req.params.id;
+  Alunas.findById(id, (err, aluna) => {
+    if (err) return res.status(500).send(err);
+
+    if (!aluna) {
+      return res.status(500).send({ message: `Não localizei a aluna de id ${id}` });
+    }
+
+    const livrosAluna = aluna.livros;
+    const livrosLidos = livrosAluna.filter(livro => livro.leu == "true");
+    const tituloLivros = livrosLidos.map(livro => livro.titulo);
+    res.status(200).send(tituloLivros);
+  });
+}
+
+exports.getSp = (req, res) => {
+  // const nasceuSp = alunas.filter(aluna => aluna.nasceuEmSp == "true")
+  Alunas.find({"nasceuEmSp": true}, (err, paulistas) => {
+    if (err) res.status(500).send(err);
+    const meninasSp = paulistas.map(aluna => aluna.nome);
+    console.log(paulistas);
+    res.status(200).send(meninasSp);
+  });
+}
+
+exports.getAge = (req, res) => {
+  const id = req.params.id;
+  Alunas.findById(id, (err, aluna) => {
+    if (err) return res.status(500).send(err);
+    if (!aluna) {
+      return res.status(500).send({ message: `Não localizei a aluna de id ${id}` });
+    }
+    const dataNasc = aluna.dateOfBirth
+    const arrData = dataNasc.split("/")
+    const dia = arrData[0]
+    const mes = arrData[1]
+    const ano = arrData[2]
+    const idade = calcularIdade(ano, mes, dia)
+    res.status(200).send({ idade })
+  })
+}  
+
+/*
 exports.getById = (req, res) => {
   const id = req.params.id
   if (id > 34 || id <= 0) {
@@ -60,6 +123,7 @@ exports.getAge = (req, res) => {
   const idade = calcularIdade(ano, mes, dia)
   res.status(200).send({ idade })
 }
+*/
 
 function calcularIdade(anoDeNasc, mesDeNasc, diaDeNasc) {
   const now = new Date()
